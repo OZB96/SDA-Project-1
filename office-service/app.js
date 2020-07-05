2ecf2a7c947a1d61d4741fb8ef7218402d685c6d
Omar Bazaid <omarzbazaid@gmail.com>
2020-07-05 11:34:43 +0000

    fixing app.js

--- office-service/app.js
+++ office-service/app.js
@@ -6,12 +6,9 @@ var path = require('path');
 //app.use(express.static(__dirname));
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'project-assessment-site/public')));
-router.get('/', function(req, res, next) q{
+app.use(express.static(__dirname));
+router.get('/office/', function(req, res, next) {
   res.render('index');
-});
-router.get('/ss', function(req, res, next) {
- res.send('Welcome to Marvel World');
 });
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
